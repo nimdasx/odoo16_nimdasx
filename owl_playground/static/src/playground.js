85d81e0fd1b2a6e7dcb5d52bdbdf668164ce5f31
dev <dev@dev>
2023-10-03 19:03:03 +0700

    3. A todo component

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,9 +1,10 @@
 /** @odoo-module **/
 
 import {Counter} from "./counter/counter";
+import {Todo} from "./todo/todo";
 import {Component} from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
-    static components = {Counter};
+    static components = {Counter,Todo};
 }
--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -3,5 +3,9 @@
 import {Component, useState} from "@odoo/owl";
 
 export class Todo extends Component {
+    static template = "owl_playground.todo";
 
+    setup() {
+        this.ngopo = {id: 3, description: "buy milk", done: false};
+    }
 }
