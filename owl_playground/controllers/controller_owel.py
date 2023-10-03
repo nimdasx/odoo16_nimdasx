a4a6e6fc2dba819b09b43fae8a26b398a9245878
dev <dev@dev>
2023-10-03 08:15:52 +0700

    owl reset

--- owl_playground/controllers/controller_owel.py
+++
@@ -1,9 +0,0 @@
-from odoo import http
-from odoo.http import request, route
-
-
-class OwlControllerOwel(http.Controller):
-
-    @http.route(['/owel_dolanan/mendem'], type='http', auth='public')
-    def show_ngantuk(self):
-        return request.render('owl_playground.lele')
--- owl_playground/controllers/controllers.py
+++ owl_playground/controllers/controllers.py
@@ -1,7 +1,6 @@
 from odoo import http
 from odoo.http import request, route
 
-
 class OwlPlayground(http.Controller):
     @http.route(['/owl_playground/playground'], type='http', auth='public')
     def show_playground(self):
@@ -9,3 +8,6 @@ class OwlPlayground(http.Controller):
         Renders the owl playground page
         """
         return request.render('owl_playground.playground')
+
+
+
