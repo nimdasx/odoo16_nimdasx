583f7764c188774c576ad2502b29b1f237f0f564
dev <dev@dev>
2023-10-11 17:14:04 +0700

    mebuh

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -5,7 +5,15 @@ import {Component, useState} from "@odoo/owl";
 export class Todo extends Component {
     static template = "owl_playground.todo";
 
+    //iki rung iso
+    static props = {
+        'id': {type: Number},
+        'description': {type: String},
+        'done': {type: Boolean}
+    };
+
     setup() {
         this.ngopo = {id: 3, description: "buy milk", done: false};
     }
+
 }
